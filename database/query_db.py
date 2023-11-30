f4fb8ecb9c25bbc8786baa93cdc461e060a966d2
TarekThomasBounassif <89601736+TarekThomasBounassif@users.noreply.github.com>
2023-11-30 22:22:53 +0100

    Test

--- database/query_db.py
+++ database/query_db.py
@@ -56,24 +56,6 @@ def insert_into_table(data_to_insert, table_name):
         cursor.close()
         connection.close()
 
-def generate_ddl_from_dataframe(
-    df: pd.DataFrame, 
-    table_name: str, 
-    primary_key: Optional[str] = None) -> str:
-
-    ddl = f"CREATE TABLE {table_name} (\n"
-
-    for column_name, dtype in df.dtypes.items():
-        sql_type = TYPE_MAPPING.get(dtype.name.lower(), dtype.name.upper())
-        ddl += f"  {column_name} {sql_type},\n"
-
-    if primary_key:
-        ddl += f"  PRIMARY KEY ({primary_key})\n"
-
-    ddl = ddl.rstrip(',\n')
-    ddl += "\n);"
-
-    return ddl
 
 data_to_create_table = pd.DataFrame({
     'id': [1, 2, 3],
